66846d765dad28ee269cbe56e1aafb35156c69ef
Anibal Valter Muchanga <valteranibal1@gmail.com>
2023-10-25 21:11:48 +0200

    Front-End
    Adicionei algumas ideias de paginas para o estudante, sô para ter uma visão geral

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace sageb.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult LivrosEmprestados()
+        {
+            return View();
+        }
+
+        public IActionResult DetalhesDaConta()
+        {
+            return View();
+        }
+    }
+}
